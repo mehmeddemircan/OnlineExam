261049ce06560e7f244e0bf238e5105b01206dd3
mehmet demircan <mehmeddemircan@gmail.com>
2023-08-29 22:18:34 +0300

    staged changes

---
+++ OnlineExam.Entities/Concrete/Questions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineExam.Entities.Concrete
+{
+
+
+    public class Questions
+    {
+
+        public string Name { get; set; }
+
+    }
+}
